39584d132df9cab0be193b88eb86071a6fdacfee
Shubham <shubhamlahase@hmail.com>
2023-07-29 11:16:57 +0530

    created pages and components needed ans set up the routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/post/:id", element: <Single /> },
+      { path: "/writeblog", element: <Write /> },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-        HII
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
