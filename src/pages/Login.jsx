66f76bb487d4a14c7861e865070867c390eea4bb
Shubham <shubhamlahase@hmail.com>
2023-07-30 12:44:02 +0530

    register and login done successfully testing on server api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: ""
+  })
+
+  const [err,setErr] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  // console.log(inputs);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post("api/auth/login", inputs);
+      // console.log(res);
+      if(res.statusText === "OK"){
+        navigate("/");
+      }
+    }catch(err){
+      setErr(err.response.data)
+      console.log("Error while Registering user:>",err);
+    }
+  }
   return (
     <div className='auth'>
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder='username'/>
-        <input required type="text" placeholder='passowrd' />
-        <button>Login</button>
-        <p>This is an error!</p>
+        <input required type="text" placeholder='username' name='username' onChange={handleChange} />
+        <input required type="text" placeholder='password' name='password' onChange={handleChange} />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>Don't you have an account?<Link to="/register">Register</Link></span>
       </form>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  const [err,setErr] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  // console.log(inputs);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post("api/auth/register", inputs);
+      // console.log(res);
+      if(res.statusText === "OK"){
+        navigate("/login");
+      }
+    }catch(err){
+      setErr(err.response.data)
+      console.log("Error while Registering user:>",err);
+    }
+  }
+
   return (
     <div className="auth">
-      <h1>Login</h1>
+      <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="text" placeholder="passowrd" />
-        <input required type="text" placeholder="email" />
-        <button>Login</button>
-        <p>This is an error!</p>
+        <input required type="text" placeholder="username" name="username" onChange={handleChange} />
+        <input required type="text" placeholder="email" name="email" onChange={handleChange} />
+        <input required type="text" placeholder="password" name="password" onChange={handleChange} />
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p>}
         <span>
           Already have an account?<Link to="/login">Login</Link>
         </span>
